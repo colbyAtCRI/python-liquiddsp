d365871355d0a7067c87ca45ea474972de01d2a8
Paul Colby <paulccolby@earthlink.net>
2024-02-27 13:36:28 -0800

    added default resamplers

--- resampler.hpp
+++ resampler.hpp
@@ -1,6 +1,74 @@
 #pragma once
 #include "liquiddsp.hpp"
 
+class RResampler
+{
+    float       mRate;
+    resamp_rrrf mSampler;
+public:
+
+    RResampler (float rate) : mRate(rate) 
+    {
+        mSampler = resamp_rrrf_create_default (mRate);
+    }
+
+   ~RResampler (void) 
+    {
+        resamp_rrrf_destroy (mSampler);
+    }
+
+    void reset (void) 
+    {
+        resamp_rrrf_reset (mSampler);
+    }
+
+    py::array_t<float> execute (py::array_t<float> inp) 
+    {
+        int no = (int)(py::len(inp)*mRate) + 1;
+        float y[no];
+        float *x = array_to_ptr<float> (inp);
+        unsigned int nw;
+        resamp_rrrf_execute_block (mSampler,x,py::len(inp),y,&nw);
+        py::array_t<float> ret(nw);
+        float *z = array_to_ptr<float>(ret);
+        std::copy (y,y+nw,z);
+        return ret;
+    }
+};
+
+class CResampler 
+{
+    float       mRate;
+    resamp_crcf mSampler;
+public:
+
+    CResampler (float rate) : mRate(rate)
+    {
+        mSampler = resamp_crcf_create_default (mRate);
+    }
+
+   ~CResampler (void) {
+        resamp_crcf_destroy (mSampler);
+    }
+
+    void reset (void) {
+        resamp_crcf_reset (mSampler);
+    }
+
+    py::array_t<std::complex<float>> execute (py::array_t<std::complex<float>> inp) {
+        int no = (int)(py::len(inp)*mRate)+1;
+        std::complex<float> y[no];
+        std::complex<float> *x = array_to_ptr<std::complex<float>> (inp);
+        unsigned int nw;
+        resamp_crcf_execute_block (mSampler, x, py::len(inp), y, &nw);
+        py::array_t<std::complex<float>> ret(nw);
+        std::complex<float> *z = array_to_ptr<std::complex<float>> (ret);
+        std::copy (y,y+nw,z);
+        return ret;
+    }
+
+};
+
 class RealResampler
 {
     float       mRate;
